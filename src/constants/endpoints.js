c309a2c96e868c51316cc378ead0f773720cee09
Ashish <ashish2199@gmail.com>
2022-08-19 00:36:01 +0530

    Stopped tracking .env File

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,4 +1,4 @@
-export const PUBLIC_KEY = "d0f5fbdac1ed61fc0de528a9cb2461e958df9566";
+export const PUBLIC_KEY = "f712dc422d2d3df45fe659097176b5f2";
 export const PRIVATE_KEY = process.env.REACT_APP_PRIVATE_API_KEY;
 export const DEFAULT_LIMIT = 100;
 export const DEFAULT_GRID_ITEMS_LIMIT = 20;
